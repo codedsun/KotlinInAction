8ea3ac769cc58ce79dafc3953c9a8ba0a78d0645
Suneet Srivastava <suneetbond91@gmail.com>
2018-05-26 22:50:45 +0530

    Functions

---
+++ src/Basics/Function.kt
@@ -0,0 +1,13 @@
+package Basics
+
+fun max(var1: Int, var2 :Int) :Int{
+    return if(var1>var2) var1 else var2
+}
+
+// Another way of declaring function
+//Function is an expression here hence we have evaluated the braces
+fun max1(a : Int, b:Int ):Int = if(a>b) a else b
+
+fun main(args: Array<String>){
+    print(max(1,2))
+}
