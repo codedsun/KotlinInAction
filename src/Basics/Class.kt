b75499cefd042aaba1cbb144bba2afee9858e455
Suneet Srivastava <suneetbond91@gmail.com>
2018-05-30 22:19:32 +0530

    When Case used

--- src/Basics/Class.kt
+++ src/Basics/Class.kt
@@ -3,7 +3,7 @@ package Basics
 class Person(val Name:String, var isMarried :Boolean)
 
 fun main(args:Array<String>){
-    var person: Person = Person("Suneet", false);
+    var person: Person = Person("Suneet", false)
     println("${person.Name} -- Name +  isMarried -- ${person.isMarried}") //getter
     person.isMarried = true //setter
     println(person.isMarried)
--- src/Basics/Enums.kt
+++ src/Basics/Enums.kt
@@ -11,4 +11,40 @@ enum class Color(var r:Int, var b: Int, var g: Int){
 }
 fun main(args:Array<String>)  {
     println(Color.RED.rgb())
+    //println(getMnemonics(Color.RED))
+   // println(getMnemonics(Color.ORANGE))
+    println(mix(Color.ORANGE,Color.YELLOW))
+    println(mixOptimized(Color.YELLOW,Color.RED))
+}
+
+//Use case of when
+
+fun getMnemonics(color:Color):String{
+    when(color){
+        Color.ORANGE,Color.YELLOW,Color.RED -> return "ALL SELECTED" //for fun, chck the output
+        Color.RED -> return "Red"
+        Color.YELLOW -> return "Yellow"
+        Color.ORANGE -> return "Orange"
+
+    //No Breaks used, only that branch is executed
+        //We can combile colors in branch
+        Color.ORANGE,Color.YELLOW,Color.RED -> return "ALL SELECTED"
+    }
+}
+
+fun mix(c1: Color, c2:Color) {
+    when(setOf(c1,c2)){
+        setOf(Color.RED,Color.YELLOW) -> println(Color.ORANGE)
+        setOf(Color.YELLOW,Color.ORANGE) -> println(Color.RED)
+    }
+}
+
+fun mixOptimized(c1 : Color, c2: Color){
+    when{
+        ((c1==Color.RED|| c2 == Color.YELLOW) || (c1 == Color.YELLOW|| c2 ==Color.RED)) -> println(Color.ORANGE)
+        ((c1==Color.YELLOW||c2==Color.ORANGE) || (c1==Color.ORANGE || c2==Color.YELLOW)) -> println(Color.RED)
+
+        else -> println("Bad color")
+
+    }
 }
